c3707d8a9c1f547a73c2e5b80f7093c18fc8e943
Enrique <enrique.reyesmarin@gmail.com>
2020-12-07 09:51:26 -0500

    Agregar archivos de proyecto.

---
+++ Numeros/Cheque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros
+{
+    public partial class Cheque : Form
+    {
+        public Cheque()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Numeros.NumerosLetras.testUnidades();
+        }
+    }
+}
